a353d2fcca6ac741a462bbfcb0861d8f085fb29f
coco <corantin.antony@gmail.com>
2026-04-29 14:50:59 +0200

    Add projects

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -3,6 +3,10 @@ import vibzImage from '../assets/img/vibz.jpg'
 import blackJackImage from '../assets/img/BlackJack_acceuil.jpg'
 import Glpi from '../assets/img/Glpi.png'
 import sportSio from '../assets/img/sportSio.jpg'
+import projetJeux from '../assets/img/ProjetJeux.png'
+import ProjetCalcul from '../assets/img/ProjetCalcul.jpg'
+import Cannrd from '../assets/img/Cannard.png'
+import zenGarden from '../assets/img/zengarden.jpg'
 
 export const projects: Project[] = [
   {
@@ -30,10 +34,10 @@ export const projects: Project[] = [
     demoUrl: '#',
   },
   {
-    title: 'Projet 6',
-    description: 'Description du projet 6.',
-    image: 'https://images.unsplash.com/photo-1497366750009-60f360e2468a?w=800&q=80',
-    tags: ['Tag1', 'Tag2', 'Tag3'],
+    title: 'Stage développeur web',
+    description: "Développement d'une application web avec Angular (interne a Michelin) qui a pour but d'aider les développeurs au quotidien notamment en leurs proposant des modèles ou de la documentation.",
+    image: 'https://images.unsplash.com/photo-1517694712202-14dd9538aa97?w=800&q=80',
+    tags: ['Angular', 'Web', 'Typescript'],
     codeUrl: '#',
     demoUrl: '#',
   },
@@ -53,4 +57,36 @@ export const projects: Project[] = [
     codeUrl: '#',
     demoUrl: '#',
   },
+  {
+    title: 'AP Maintenance corrective et/ou évolution de jeux',
+    description: "Ce projet en C# regroupe trois jeux classiques réunis dans une même application avec interface graphique. Il propose le Pendu, Mastermind et le Morpion, accessibles depuis un menu commun. L’ensemble offre une expérience simple et intuitive, permettant de jouer à ces jeux emblématiques dans une version modernisée. Le projet met en avant la structuration du code, la gestion des interactions et la création d’une interface claire.",
+    image: projetJeux,
+    tags: ['C#', 'Windows Forms', 'Jeux classiques'],
+    codeUrl: 'https://github.com/coco324/ProjetJeux',
+    demoUrl: '#',
+  },
+  {
+    title: 'Logiciel Apprentissage calcul',
+    description: "Cette application propose un entraînement simple et rapide aux opérations mathématiques essentielles. Elle met à disposition des exercices d’addition, de soustraction et de multiplication pour renforcer la maîtrise des calculs de base. L’interface est pensée pour être accessible et intuitive. L’objectif est d’aider l’utilisateur à améliorer sa rapidité et sa précision en mathématiques.",
+    image: ProjetCalcul,
+    tags: ['C#', 'Windows Forms', 'Apprentissage'],
+    codeUrl: 'https://github.com/coco324/CalculEnfant',
+    demoUrl: '#',
+  },
+  {
+    title: 'Journal - Canard Connecté ',
+    description: "Chaque semaine au début de notre année, un groupe de 4 personne présenté des informations d'actualité sur l'informatique en général. On pouvais parler du sujet de notre choix.",
+    image: Cannrd,
+    tags: ['veille technologique', 'actualité', 'communication'],
+    codeUrl: '#',
+    demoUrl: '#',
+  },
+  {
+    title: 'CSS Zen Garden',
+    description: "Le site CSS Zen Garden présente un fichier HTML fixe dont l’apparence est entièrement transformée grâce au CSS. L’objectif est de montrer comment une simple feuille de style peut modifier complètement l’ambiance du site sans toucher au contenu.",
+    image: zenGarden,
+    tags: ['css', 'design', 'web'],
+    codeUrl: 'https://github.com/coco324/ZenGarden',
+    demoUrl: '#',
+  }
 ]
